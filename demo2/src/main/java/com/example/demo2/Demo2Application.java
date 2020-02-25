71fbbb008700b6c4782ea8829de50c47032b4e45
up1 <somkiat.p@gmail.com>
2020-02-25 16:22:05 +0700

    Working with wiremock (Gateway testing)

--- demo2/src/main/java/com/example/demo2/Demo2Application.java
+++ demo2/src/main/java/com/example/demo2/Demo2Application.java
@@ -2,7 +2,10 @@ package com.example.demo2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 import static java.lang.System.*;
 
@@ -17,6 +20,11 @@ public class Demo2Application {
 			out.println(bean);
 		}
 		out.println(context.getBeanDefinitionCount());
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplateBuilder().build();
 	}
 
 }
